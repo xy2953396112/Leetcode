128a0507dbda76882a4ea3a67efc9ab0ab6c1b7e
xzh <953396112@qq.com>
2018-03-29 10:04:53 +0800

    Leetcode

---
+++ main/java/Test/TestCharAt.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class TestCharAt {
+
+     public static void main(String[] args){
+
+
+         String abc = "abc";
+
+          int i=  abc.charAt(0);
+
+          System.out.println(i+"");
+     }
+}
